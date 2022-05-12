08bba7e3ff5c5d62c7ce36da47aa69bb25c0a4ec
jo-ui <57154082+jo-ui@users.noreply.github.com>
2022-05-12 23:21:56 +0300

    DB

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -11,7 +11,7 @@ const DB = process.env.DATABASE.replace(
 );
 
 mongoose
-  .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
