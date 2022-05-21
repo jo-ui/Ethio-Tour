311fd92a0f253092b7cfb313f3373a1f5a62b6f0
jo-iv <57154082+jo-ui@users.noreply.github.com>
2022-05-21 22:26:11 +0300

    update current user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,6 +22,34 @@ exports.getUser = (req, res) => {
   });
 };
 
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
