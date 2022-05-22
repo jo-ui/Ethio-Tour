373457bffbd2c140221102db251e59edb00c3f60
jo-iv <57154082+jo-ui@users.noreply.github.com>
2022-05-22 14:33:11 +0300

    rate limiting

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
