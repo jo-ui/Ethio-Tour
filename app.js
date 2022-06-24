eb8f2262a12d60955909434dbb9261e7141419fc
jo-iv <57154082+jo-ui@users.noreply.github.com>
2022-06-24 23:25:35 +0300

    bug

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
-const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -21,7 +20,6 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
-app.use(cors());
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
