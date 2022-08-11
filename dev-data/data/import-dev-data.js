45f6a824941c94138407713916c279a34e6e8b91
jo-iv <57154082+jo-ui@users.noreply.github.com>
2022-08-11 23:59:39 +0300

    Merge branch 'main' of https://github.com/jo-ui/Ethio-Tour into main

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -14,7 +14,7 @@ const DB = process.env.DATABASE.replace(
 
 mongoose
   //process.env.DATABASE_LOCAL
-  .connect(DB,{
+  .connect(process.env.DATABASE_LOCAL,{
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
