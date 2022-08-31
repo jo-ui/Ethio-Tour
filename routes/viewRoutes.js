07cd6cdfd041dc95c709565ed5a50272792ab7ee
jo-iv <57154082+jo-ui@users.noreply.github.com>
2022-08-31 19:25:36 +0300

    Webhook Checkout

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -14,7 +14,7 @@ router.get('/me', authController.protect, viewsController.getAccount);
 
 router.get(
   '/my-tours',
-  bookingController.createBookingCheckout,
+  // bookingController.createBookingCheckout,
   authController.protect,
   viewsController.getMyTours
 );
