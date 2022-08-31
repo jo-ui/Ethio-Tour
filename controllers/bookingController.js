877c7ffaf76d5ab8e245c8b460b36740acef4f29
jo-iv <57154082+jo-ui@users.noreply.github.com>
2022-08-31 20:27:17 +0300

    check

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -76,9 +76,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.object.amount_subtotal / 100;
   await Booking.create({ tour, user, price });
-  console.log(tour, user, price);
 };
 
 exports.webhookCheckout = (req, res, next) => {
